91909bb4438fa01be492dbadac34943402519849
johnconn <john.connolly@mekaworks.com>
2020-05-05 18:15:26 -0500

    sim changes

--- launch/ekf.launch.py
+++ launch/ekf.launch.py
@@ -1,5 +1,4 @@
 # Copyright 2018 Open Source Robotics Foundation, Inc.
-# Copyright 2019 Samsung Research America
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+""" ekf launch.py """
+
+import launch
 from launch import LaunchDescription
-from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 import os
 import yaml
@@ -22,14 +23,77 @@ from launch.substitutions import EnvironmentVariable
 import pathlib
 import launch.actions
 from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
-    return LaunchDescription([
-        launch_ros.actions.Node(
-            package='robot_localization',
-            node_executable='ekf_node',
-            node_name='ekf_filter_node',
+    parameters_file_dir = pathlib.Path(os.path.join(get_package_share_directory('robot_localization'), 'params'))
+    print(f"params file dir is {parameters_file_dir}")
+    parameters_file_path = parameters_file_dir / 'ekf.yaml'
+    print(f"params file path is {parameters_file_path}")
+    os.environ['FILE_PATH'] = str(parameters_file_dir)
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    sim_arg = launch.actions.DeclareLaunchArgument(
+            'use_sim_time', default_value='false',
+            description='Use simulation (Gazebo) clock if true')
+
+
+    meka_model = 'big_meka'
+
+    urdf_file_name = meka_model + '.urdf'
+    xacro_file_name = meka_model + '.urdf.xacro'
+    big_meka_dir = os.path.join(get_package_share_directory('mbot4_gazebo'), 'models',meka_model) 
+    urdf_full_name = os.path.join(big_meka_dir, urdf_file_name)
+
+    robot_publisher = launch_ros.actions.Node(
+            package='robot_state_publisher',
+            node_executable='robot_state_publisher',
+            node_name='robot_state_publisher',
             output='screen',
-            parameters=[os.path.join(get_package_share_directory("robot_localization"), 'params', 'ekf.yaml')],
-           ),
-])
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=[urdf_full_name])
+
+         #*****test_ekf_localization_node_interfaces.test***** 
+    se_node_odom = launch_ros.actions.Node(
+            package='robot_localization', node_executable='se_node', node_name='ekf_se_odom',
+            output='screen',
+            parameters=[
+                parameters_file_path,
+                str(parameters_file_path),
+                [EnvironmentVariable(name='FILE_PATH'), os.sep, 'mekaworks_navsat.yaml'], {'use_sim_time': use_sim_time}
+                ],
+            remappings=[('/odometry/filtered', '/odometry/odom_se'),('/odometry/filtered/pose', '/odometry/odom_se/pose') ]
+            )
+
+    se_node_map = launch_ros.actions.Node(
+            package='robot_localization', node_executable='se_node', node_name='ekf_se_map',
+            output='screen',
+            parameters=[
+                parameters_file_path,
+                str(parameters_file_path),
+                [EnvironmentVariable(name='FILE_PATH'), os.sep, 'mekaworks_navsat.yaml'], {'use_sim_time': use_sim_time}
+                ],
+            remappings=[('/odometry/filtered', '/odometry/map_se'),('/odometry/filtered/pose', '/odometry/map_se/pose') ]
+            )
+    navsat_node = launch_ros.actions.Node(
+            package='robot_localization', node_executable='navsat_transform_node', node_name='navsat_transform',
+            output='screen',
+            parameters=[
+                parameters_file_path,
+                str(parameters_file_path),
+                [EnvironmentVariable(name='FILE_PATH'), os.sep, 'mekaworks_navsat.yaml'], {'use_sim_time': use_sim_time}
+                ],
+            remappings=[('/gps/fix', '/gps0/fix'), 
+                ('/imu', '/imu/filter_data'), 
+                ('/odometry/filtered', '/odometry/map_se')
+                ]
+            )
+
+    return LaunchDescription([
+        #robot_publisher,
+        navsat_node,
+        se_node_odom,
+        se_node_map
+        ])
